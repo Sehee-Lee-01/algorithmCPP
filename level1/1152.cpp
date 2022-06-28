ae8b1fe20181ccc87c93269e6b0a7af7b59b85c6
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-06-28 09:22:20 +0900

    1152 fin

--- level1/1152.cpp
+++ level1/1152.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+string words, str = " ";
+size_t pos = 0;
+int count = 0;
+
+int main(void) {
+    getline(cin, words);
+    while (pos != string::npos) {   
+        if(words.size()==1 && words.substr(0,1)==" ") {break;}
+        else if ((pos == 0) && (words.substr(pos,1)==" ")) {
+            if((pos + 1 == 1) && (words.substr(pos+1,1)!=" ")) {count=1;}
+        }
+        else if ((pos == 0) && (words.substr(pos,1)!=" ")) {count = 1;}
+        else if ((pos == words.size()-1) && (words.substr(pos,1)==" ")) {}
+        else if (words.substr(pos,1)==" ") {count++;}
     
+        if (pos == words.size()) {break;}
+        else {pos++;}
+    }
+    cout <<  count << endl;
 }
