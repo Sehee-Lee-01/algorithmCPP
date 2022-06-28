cbec4e5c1c1bbc2a2297bee7522e222cfc89b94d
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-06-28 17:14:30 +0900

    fin 2438

---
+++ level1/2438.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int input;
+
+int main() {
+    scanf("%d", &input);
+    for (int i=0; i<input; i++) {
+        for(int j=0; j<i+1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
