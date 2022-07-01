bfb7847f0e09acdf3e0de7089042c2a716f24ac6
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-07-02 02:55:13 +0900

    fin 1436

--- class2/1436.cpp
+++ class2/1436.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
-#include <cmath>
-#include <set>
 #include <string>
-#include <algorithm>
 using namespace std;
-# define MAX_N 10000
-int N, counter, title_len=1; // len - 3
-set<int> titles;
-string temp, jongmal="666";
+int N, counter, title=665;
+string temp;
 int main() {
-    cin >> N;
-    while (titles.size() < MAX_N)
+    scanf("%d", &N);
+    while (counter!=N)
     {
-        for(int j=0; j<=title_len; j++) {
-            for (int i=0; i<pow(10,title_len); i++) {
-                temp = to_string(i);
-                while (temp.length()==title_len-1){temp.insert(0,"0");}
-                if (j == title_len) {temp += jongmal;}
-                else {temp.insert(j,jongmal);}
-                titles.insert(stoi(temp));
-            cout << "nana? "<< temp << " " << j << endl;
-            }
-        }
-        title_len++;
+       title++;
+       temp=to_string(title);
+       if (temp.find("666") != string::npos) {counter++;}
     }
-    cout << "nana?" << endl;
-    for (auto iter=titles.begin(); iter!=titles.end();iter++) {
-        if (counter+1==N) {cout << *iter << " ";break;}
-        counter++;
-    }
+    printf("%d", title);
 }
