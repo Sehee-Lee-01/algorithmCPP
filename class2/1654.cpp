4d01c41ce1db58256770c8de6f4163872fc51f10
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-07-04 01:29:45 +0900

    fin 1654

--- class2/1654.cpp
+++ class2/1654.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 #include <vector>
 using namespace std;
 
-int K, N, X, limit_X = 1, temp, lan_count;
+unsigned int K, N, X, temp, lan_count, limit_X = 1;
 
 int main() {
     scanf("%d %d", &K, &N);
-    vector<int> lan_length;
+    vector<unsigned int> lan_length;
     for (int i=0; i<K; i++) {
         scanf("%d", &temp);
         lan_length.push_back(temp);
     }
     sort(lan_length.begin(), lan_length.end());
     X = lan_length.back();
-    while (1)
+    while (limit_X <= X)
     {
         temp = (X+limit_X)/2;
+        // printf("lan count %d limit_X %d X %d temp(mid) %d\n", lan_count, limit_X, X, temp);
         lan_count = 0;
         for (auto iter=lan_length.begin(); iter!=lan_length.end();iter++) {lan_count += *iter/temp;}
-        if (X < limit_X) {break;}
-        else if(lan_count<N) {X = temp-1;} 
-        else{limit_X = temp+1;}
+        
+        if(lan_count<N) {X = temp-1;} 
+        else {limit_X = temp+1;}
     }
-    
+
     printf("%d\n", X);
 }
