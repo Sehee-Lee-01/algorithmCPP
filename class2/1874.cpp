ae1d63422eeb7cc76552c11dbccf7cca557908b3
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-07-04 11:31:04 +0900

    fin 1874

--- class2/1874.cpp
+++ class2/1874.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <stack>
+#include <vector>
 using namespace std;
-
+int testcase, temp, curr=1;
+vector<char> result;
+stack<int> sequence; 
 int main() {
-    
+    sequence.push(0);
+    scanf("%d", &testcase);
+    for (int i=0; i<testcase; i++) {
+        scanf("%d", &temp);
+        if (temp >= curr) {
+            while (curr <= temp) {sequence.push(curr++); result.push_back('+');}
+            sequence.pop(); result.push_back('-');
+        }
+        else {
+            if(sequence.top()!=temp) {result.push_back('+'); break;}
+            else {sequence.pop(); result.push_back('-');}
+        }
+    }
+    if (result.back() != '-') {printf("NO\n");}
+    else {for(auto &s : result) {printf("%c\n", s);}}
 }
