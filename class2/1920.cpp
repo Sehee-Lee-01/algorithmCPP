416b6e276c15ac87ec153ad3373a310f73ded087
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-07-04 18:53:05 +0900

    fin 1920

--- class2/1920.cpp
+++ class2/1920.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <set>
 using namespace std;
 
+int N, temp, M;
+set<int> A;
+
 int main() {
+    scanf("%d", &N);
+    for (int i=0; i<N; i++) { scanf("%d", &temp); A.insert(temp); }
     
+    scanf("%d", &M); 
+    for (int i=0; i<M; i++) { 
+        scanf("%d", &temp); 
+        if (A.find(temp)==A.end()) {printf("0\n");}
+        else {printf("1\n");}
+    }
 }
