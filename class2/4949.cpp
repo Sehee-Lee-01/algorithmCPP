d23b8c4b73eb50323d1e1f8fd6d3c46413f39be8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-07-06 01:44:07 +0900

    why 4949

---
+++ class2/4949.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+string input;
+bool result;
+int main() {
+    while (1) {
+        stack<char> gual_open; result=true;
+        getline(cin, input); if(input == "") break;
+        
+        for (auto &c : input) {
+            if ((c == '(')||(c == '[')) {gual_open.push(c); continue;}
+            
+            if (c == ')') {
+                if ((!gual_open.empty())&&(gual_open.top() == '(')) {gual_open.pop(); continue;}
+                else {result=false; break;}
+            }
+            if (c == ']') {
+                if ((!gual_open.empty())&&(gual_open.top() == '[')) {gual_open.pop(); continue;}
+                else {result=false; break;}            
+            }
+            if (c == '.') {
+                if (gual_open.empty()) {break;} else {result=false; break;}
+            }
+        }
+
+        if(result) {printf("yes\n");} else {printf("no\n");}
+    }
+}
