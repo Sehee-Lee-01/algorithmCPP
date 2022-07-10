f6e1a7c8b358685c1a5f2c30921e50522732d0ef
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-07-11 00:17:07 +0900

    fin 2798

--- class2/2798.cpp
+++ class2/2798.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int N, M, temp;
+int N, M, temp, sum, max_sum;
+vector<int> input;
 int main() {
     scanf("%d %d", &N, &M);
     for (int i=0; i<N; i++) {
-        scanf("%d", temp);
-        
+        scanf("%d", &temp);
+        input.push_back(temp);
     }
+    for(int i=0; i<input.size()-2; i++) {
+        for(int j=i+1; j<input.size()-1; j++) {
+            for(int k=j+1; k<input.size(); k++) {
+                sum = input[i]+input[j]+input[k];
+                if((sum > max_sum)&&(sum<=M)) {
+                    
+                    // printf("%d %d %d\n", input[i],input[j],input[k]);
+                    max_sum=sum;
+                }
+            }
+        }
+    }
+    printf("%d\n", max_sum);
 }
