21e21d14aed25d8f91155dbaefce467f19a3a9ff
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-07-12 15:53:22 +0900

    fin 1978

--- class2/1978.cpp
+++ class2/1978.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-
+bool so_su[1001]; 
+int N, temp, sum;
 int main() {
-    
+    // so_su[0]=false;so_su[1]=false;
+    for(int i=2; i<1001; i++){so_su[i] = true;}
+    for (int i=2; i<501; i++) {
+        int mul = 2;
+        while ((mul*i)<1001){so_su[mul++*i] = false;}
+    }
+    scanf("%d", &N);
+    for (int i=0; i<N; i++) {
+        scanf("%d", &temp);
+        if (so_su[temp]) {sum++;}
+    }
+    printf("%d", sum);
 }
