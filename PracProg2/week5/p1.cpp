41840f83acec17d43dcb62310c3854602e49ffa9
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-10-07 01:33:22 +0900

    add: week5..모르겠습니다...

--- PracProg2/week5/p1.cpp
+++ PracProg2/week5/p1.cpp
@@ -1,35 +1,22 @@
+// -
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
+typedef long long ll;
+
 int main()
 {
     int T, L;
-    char temp;
-    scanf("%d", &T);
+    string temp;
+    cin >> T;
     while (T--)
     {
-        scanf("%d", &L);
-        vector<char> calculater;
-        vector<int> brace_locations;
-
-        for (int i = 0; i < L; i++)
+        cin >> L;
+        while (L--)
         {
-            scanf("%c", &temp);
-            calculater.push_back(temp);
-            if (temp == '(')
-                brace_locations.push_back(i);
-        }
-
-        while (!brace_locations.empty())
-        {
-            int curr_location = brace_locations.back();
-            brace_locations.pop_back();
-            int curr_calculate = 0;
-            while (calculater[curr_location] == ')')
-            {
-                if (calculater[curr_location] == '(')
-                    continue;
-            }
+            cin >> temp;
+            cout << "temp" << T << ": " << temp << '\n';
         }
     }
 }
--- PracProg2/week5/p2.cpp
+++ PracProg2/week5/p2.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-typedef unsigned long long ull;
+typedef long long ll;
 
 int main()
 {
-    ull t, d, temp, run1, run2, income = 0;
-    vector<ull> stock_price;
-    scanf("%d", &t);
+    int t;
+    cin >> t;
     while (t--)
     {
-        scanf("%d", &d);
+        ll d, run1, run2, temp, income = 0;
+        vector<ll> stock_price;
+        cin >> d;
         run1 = d, run2 = d;
         while (run1--)
         {
-            scanf("%d", &temp);
+            cin >> temp;
             stock_price.push_back(temp);
         }
+
+        ll max_price = stock_price[stock_price.size() - 1];
+
         while (run2--)
         {
-            if (run2 > 1)
-            {
-                // 마지막은 바로 앞 거 높은지 보기
-                if (stock_price[run2 - 1] > stock_price[run2 - 2])
-                {
-                    curr_max = stock_price[run2 - 1];
-                }
-            }
+            if (max_price < stock_price[run2])
+                max_price = stock_price[run2];
+            else
+                income += (max_price - stock_price[run2]);
         }
+        cout << income << '\n';
     }
 }
