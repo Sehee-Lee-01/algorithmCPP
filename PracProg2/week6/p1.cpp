d312eb86f0cc88dcda4eded673a9b0eabc532489
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-10-13 15:39:46 +0900

    fin: week6

---
+++ PracProg2/week6/p1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t, n, m, d;
+    string temp;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m >> d;
+        int row_length = n * 2 - 1, unknown_row = -1;
+        string ladder[m];                // 미리 공간 만들기
+        for (int i = m - 1; i >= 0; i--) // 줄 갯수
+        {
+            cin >> temp;
+            ladder[i] = temp;
+            if (ladder[i][0] == '?')
+                unknown_row = i;
+        }
+        // cout << unknown_row << '\n';
+
+        // 공간 만들고, ? 행 전까지는 길 찾기 시작
+        int curr_location = 2 * d - 2;
+        for (int i = 0; i < unknown_row; i++)
+        {
+            // 왼쪽 이동이 가능할 때
+            if ((1 < curr_location) && (ladder[i][curr_location - 1] == '+'))
+                curr_location -= 2;
+            //오른쪽 이동이 가능할 때
+            else if ((curr_location < (row_length - 2)) && (ladder[i][curr_location + 1] == '+'))
+                curr_location += 2;
+            // cout << curr_location << '\n';
+        }
+
+        vector<int> curr_locations;
+        curr_locations.push_back(curr_location); // 1. 직진 길 curr_locations[1]
+        // ? 행일 때 curr_location 직진, 양 옆 확인
+        if (1 < curr_location) // 2. 왼쪽 길 만들 수 있는지 curr_locations[0]
+            curr_locations.push_back(curr_location - 2);
+
+        if (curr_location < (row_length - 2)) // 3. 오른쪽 길 만들 수 있는지 curr_locations[2]
+            curr_locations.push_back(curr_location + 2);
+
+        // cout << row_length << '\n';
+
+        // ? 행 이후로 길 찾기
+        for (int i = unknown_row + 1; i < m; i++)
+        {
+            for (int j = 0; j < curr_locations.size(); j++)
+            {
+                // 왼쪽 이동이 가능할 때
+                if ((1 < curr_locations[j]) && (ladder[i][curr_locations[j] - 1] == '+'))
+                    curr_locations[j] -= 2;
+                //오른쪽 이동이 가능할 때
+                else if ((curr_locations[j] < (row_length - 2)) && (ladder[i][curr_locations[j] + 1] == '+'))
+                    curr_locations[j] += 2;
+            }
+        }
+
+        // 정렬
+        sort(curr_locations.begin(), curr_locations.end());
+
+        // 출력
+        for (int i = 0; i < curr_locations.size(); i++)
+            cout << (curr_locations[i] + 2) / 2 << " ";
+        cout << '\n';
+    }
+}
---
+++ PracProg2/week6/p2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+void makeScoreLayer(int n, vector<vector<int>> &map, int con_y, int con_x)
+{
+    for (int j = con_y - 10; j <= con_y + 10; j++)
+    {
+        for (int k = con_x - 10; k <= con_x + 10; k++)
+        {
+            // 맵의 범위 내에 들어오고 편의점 위치가 아닌 곳을 탐색
+            if ((0 <= j) && (j < n) && (0 <= k) && (k < n) && (map[j][k] != -1))
+            {
+                if ((abs(j - con_y) + abs(k - con_x)) <= 3) // 맨하튼 거리가 3이하일 경우 +3
+                    map[j][k] += 3;
+                else if ((abs(j - con_y) + abs(k - con_x)) <= 10) // 맨하튼 거리가 3초과 10이하일 경우 +1
+                    map[j][k] += 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int t, n, c, con_y, con_x;
+    cin >> t;
+    for (int testcase = 0; testcase < t; testcase++)
+    {
+        cin >> n >> c;
+        vector<vector<int>> map(n, vector<int>(n)); //[n][n] 0으로 초기화
+
+        for (int i = 0; i < c; i++) // 입력 받으면서 지도에 점수 레이어 씌우기
+        {
+            cin >> con_y >> con_x;
+            map[con_y][con_x] = -1;
+            makeScoreLayer(n, map, con_y, con_x); // map에 1점, 2점 점수 채우기
+        }
+
+        // 최댓값, 그 값의 좌표 순회하기
+        int max_score = 0, max_y, max_x;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (max_score < map[i][j])
+                {
+                    max_score = map[i][j];
+                    max_y = i;
+                    max_x = j;
+                }
+            }
+        }
+
+        // 결과 출력
+        cout << max_y << " " << max_x << " " << max_score << '\n';
+    }
+}
