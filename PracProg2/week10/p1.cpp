f9d6b556a62c55d035fe469638170183a4345bd7
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-11-19 22:49:31 +0900

    why: 시간초과 문제 발생

--- PracProg2/week10/p1.cpp
+++ PracProg2/week10/p1.cpp
@@ -1,17 +1,21 @@
 // 삼성 코테 문제
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
     while (t--)
     {
         int n, m, temp, sung_row, sung_column;
         cin >> n >> m;
-        int map[n][n];
+        vector<vector<int>> map(n, vector<int>(n));
         // 숙소 위치 받기
         vector<pair<int, int>> room_places;
         for (int i = 0; i < n; i++)
@@ -25,59 +29,70 @@ int main()
         for (int i = 0; i < m; i++)
         {
             cin >> sung_row >> sung_column;
-            int start_time = i + 1, min_distance = n * n, min_dis_row = -1, min_dis_col = -1;
+            int min_distance = n * n, min_dis_row = -1, min_dis_col = -1;
             map[sung_row - 1][sung_column - 1] = -1; //갔던 성심당 표시
             vector<int> first_log = {sung_row - 1, sung_column - 1, 0, -1, -1};
-            vector<vector<int>> to_move = {first_log};
+            queue<vector<int>> to_move;
+            to_move.push(first_log);
             while (!to_move.empty())
             {
-                int curr_row = to_move.back()[0];
-                int curr_column = to_move.back()[1];
-                int curr_distance = to_move.back()[2];
-                int past_row = to_move.back()[3];
-                int past_column = to_move.back()[4];
-                to_move.pop_back();
+                vector<int> curr_info = to_move.front();
+                to_move.pop();
 
-                if (min_distance >= curr_distance) // 거리 같을 떼 작을 때 반영하기
+                if (map[curr_info[0]][curr_info[1]] == 1) // 숙소 도착이라면
                 {
-                    if (map[curr_row][curr_column] == 1) // 숙소 도착이라면
+                    if (min_distance == curr_info[2]) // 가까운 거리정도와 같다면
                     {
-                        if (min_dis_row == curr_row)
+                        if (min_dis_row == curr_info[0]) // 이것마저 같다면
                         {
-                            if (min_dis_col > curr_column)
-                            {
-                                min_distance = curr_distance;
-                                min_dis_col = curr_column;
-                            }
+                            if (min_dis_col > curr_info[1]) // 열이 작은거
+                                min_dis_col = curr_info[1];
                         }
-                        else
+                        else if (min_dis_row > curr_info[0]) // 행이 작은거
                         {
-                            min_distance = curr_distance;
-                            min_dis_row = curr_row;
-                            min_dis_col = curr_column;
+                            min_dis_row = curr_info[0];
+                            min_dis_col = curr_info[1];
                         }
                     }
-                    else if (min_distance > curr_distance)
-                        // 아니라면 갈 수 있는 길인지 확인하고 이동할 위치에 넣기
-                        if ((curr_row > 0) && (map[curr_row - 1][curr_column] != -1)) //상
-                            if (!((curr_row - 1 == past_row) && (curr_column == past_column)))
-                                to_move.push_back({curr_row - 1, curr_column, curr_distance + 1});
-                    if ((curr_row < n - 1) && (map[curr_row + 1][curr_column] != -1)) //하
-                        if (!((curr_row - 1 == past_row) && (curr_column == past_column)))
-                            to_move.push_back({curr_row + 1, curr_column, curr_distance + 1});
-                    if ((curr_column > 0) && (map[curr_row][curr_column - 1] != -1)) //좌
-                        if (!((curr_row - 1 == past_row) && (curr_column == past_column)))
-                            to_move.push_back({curr_row, curr_column - 1, curr_distance + 1});
-                    if ((curr_column < n - 1) && (map[curr_row][curr_column + 1] != -1)) //우
-                        if (!((curr_row - 1 == past_row) && (curr_column == past_column)))
-                            to_move.push_back({curr_row, curr_column + 1, curr_distance + 1});
+                    else if (min_distance > curr_info[2]) // 거리가 더 가깝다면
+                    {
+                        min_distance = curr_info[2];
+                        min_dis_row = curr_info[0];
+                        min_dis_col = curr_info[1];
+                    }
+                }
+
+                else if (min_distance >= (curr_info[2] + 1))
+                // 아니라면 갈 수 있는 길인지 확인하고 이동할 위치에 넣기(우선순위)
+                {
+                    if ((curr_info[0] > 0) && (map[curr_info[0] - 1][curr_info[1]] != -1)) //상
+                    {
+                        if (!((curr_info[0] - 1 == curr_info[3]) && (curr_info[1] == curr_info[4])))
+                            to_move.push({curr_info[0] - 1, curr_info[1], curr_info[2] + 1, curr_info[0], curr_info[1]});
+                    }
+                    if ((curr_info[1] > 0) && (map[curr_info[0]][curr_info[1] - 1] != -1)) //좌
+                    {
+                        if (!((curr_info[0] == curr_info[3]) && (curr_info[1] - 1 == curr_info[4])))
+                            to_move.push({curr_info[0], curr_info[1] - 1, curr_info[2] + 1, curr_info[0], curr_info[1]});
+                    }
+                    if ((curr_info[1] < n - 1) && (map[curr_info[0]][curr_info[1] + 1] != -1)) //우
+                    {
+                        if (!((curr_info[0] == curr_info[3]) && (curr_info[1] + 1 == curr_info[4])))
+                            to_move.push({curr_info[0], curr_info[1] + 1, curr_info[2] + 1, curr_info[0], curr_info[1]});
+                    }
+                    if ((curr_info[0] < n - 1) && (map[curr_info[0] + 1][curr_info[1]] != -1)) //하
+                    {
+                        if (!((curr_info[0] + 1 == curr_info[3]) && (curr_info[1] == curr_info[4])))
+                            to_move.push({curr_info[0] + 1, curr_info[1], curr_info[2] + 1, curr_info[0], curr_info[1]});
+                    }
                 }
             }
             map[min_dis_row][min_dis_col] = -1;
-            int arr_time = min_distance + start_time;
+            int arr_time = min_distance + i + 1;
+
             if (max_arr_time < arr_time)
                 max_arr_time = arr_time;
         }
-        cout << max_arr_time << "\n";
+        cout << max_arr_time << "\n"; // result
     }
 }
---
+++ PracProg2/week10/p1_re.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+// 최적경로 찾을 때 이용
+const int direc_y[] = {-1, 0, 0, 1};
+const int direc_x[] = {0, -1, 1, 0};
+
+class Person
+{
+public:
+    pair<int, int> sung_place; // y, x
+    pair<int, int> past_place = make_pair(-1, -1);
+    pair<int, int> curr_place; // y, x, 이동할 때 필요, 초기값은 숙소 위치
+    int curr_dis;              // 숙소와 성심당의 최단거리
+    // init
+    Person(const int sung_y, const int sung_x, vector<bool> &suk_using, const vector<pair<int, int>> &suk_list, const int n)
+    {
+        sung_place = make_pair(sung_y, sung_x);
+        seekSuk(suk_using, suk_list, n);
+    }
+
+    // 초기 최단거리 숙소 찾기(숙소 위치, 숙소와 성심당의 최단거리)
+    void seekSuk(vector<bool> &suk_using, const vector<pair<int, int>> &suk_list, const int n)
+    {
+        curr_dis = n * n;
+        int suk_index = -1;
+        for (int j = 0; j < suk_using.size(); j++)
+        {
+            if (!suk_using[j]) // 겹치는 지
+            {
+                int distance = abs(sung_place.first - suk_list[j].first) + abs(sung_place.second - suk_list[j].second);
+                if (curr_dis > distance)
+                {
+                    curr_dis = distance;
+                    curr_place.first = suk_list[j].first;
+                    curr_place.second = suk_list[j].second;
+                    suk_index = j;
+                }
+                else if (curr_dis == distance)
+                {
+                    if (curr_place.first > suk_list[j].first)
+                    {
+                        curr_place.first = suk_list[j].first;
+                        curr_place.second = suk_list[j].second;
+                        suk_index = j;
+                    }
+                    else if ((curr_place.first == suk_list[j].first) && (curr_place.second > suk_list[j].second))
+                    {
+                        curr_place.second = suk_list[j].second;
+                        suk_index = j;
+                    }
+                }
+            }
+        }
+        suk_using[suk_index] = true;
+        // cout << curr_dis << "(" << curr_place.first << ", " << curr_place.second << ")\n";
+    }
+    // 최적경로 찾기 in on tick(최단 거리 방향)
+    void moveRoad(const vector<vector<int>> &map, const int n)
+    {
+        pair<int, int> move;
+        int min_dis = n * n;
+        for (int j = 0; j < 4; j++)
+        {
+            int road_y = curr_place.first + direc_y[j];
+            int road_x = curr_place.second + direc_x[j];
+            // 상하좌우 칸이 범위 안에 있는지, 이동이 가능한지()-1이 표시가 안되어 있는지)
+            if (curr_dis == 1)
+            {
+                curr_dis = 0;
+                return;
+            }
+            else if (!((past_place.first == road_y) && (past_place.second == road_x))) // 이전에 갔던 곳이 아닌지
+            {
+                if ((0 <= road_y) && (road_y < n) && (0 <= road_x) && (road_x < n) && (map[road_y][road_x] != -1))
+                {
+                    int dis = abs(road_y - sung_place.first) + abs(road_x - sung_place.second);
+                    if (min_dis > dis) // 최소 거리인지
+                    {
+                        move = make_pair(road_y, road_x);
+                        min_dis = dis;
+                    }
+                }
+            }
+        }
+        past_place = curr_place;
+        curr_place = move;
+        curr_dis = min_dis;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, temp, sung_row, sung_column;
+        cin >> n >> m;
+        vector<vector<int>> map(n, vector<int>(n));
+        vector<pair<int, int>> suk_list; // 숙소 위치 받아서 맵에 표시하고 저장
+        int suk_count = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            {
+                cin >> temp;
+                map[i][j] = temp;
+                if (temp == 1) // 숙소면 숙소 리스트에 저장
+                {
+                    suk_list.push_back(make_pair(i, j));
+                    suk_count++;
+                }
+            }
+
+        // 성심당 위치와 숙소 위치 저장
+        vector<bool> suk_using(suk_count, false);
+        // cout << "suk_count " << suk_count << "\n";
+        vector<Person> person_list;
+        vector<bool> is_arrive = vector<bool>(m, false); // 성심당 도착 사람
+        int wait_num = m, arrive_num = 0;                // 사람 수
+        int time = 0;
+        vector<pair<int, int>> unable_move; // 다 움직인 직후에 숙소를 떠난 위치가 있다면 기록
+        while (arrive_num < m)              //  다 이동 완료 할 때까지
+        {
+            for (int i = 0; i < person_list.size(); i++) // 개인씩 우선 이동
+            {
+                if (!is_arrive[i]) // 도착한 사람 구분
+                {
+                    person_list[i].moveRoad(map, n);
+                    // cout << "person" << i << ": " << person_list[i].curr_dis << "\n";
+                    if (person_list[i].curr_dis == 0) // 도착이라면
+                    {
+                        unable_move.push_back(make_pair(person_list[i].sung_place.first, person_list[i].sung_place.second));
+                        is_arrive[i] = true;
+                        arrive_num++;
+                    }
+                }
+            }
+
+            while (!unable_move.empty())
+            {
+                // cout << "(" << unable_move.back().first << ", " << unable_move.back().second << ")"
+                //      << "\n";
+                map[unable_move.back().first][unable_move.back().second] = -1;
+                unable_move.pop_back();
+            }
+            if (wait_num)
+            {
+                cin >> sung_row >> sung_column;
+                person_list.push_back(Person(sung_row - 1, sung_column - 1, suk_using, suk_list, n));
+                map[person_list[person_list.size() - 1].curr_place.first][person_list[person_list.size() - 1].curr_place.second] = -1;
+                // cout << m - wait_num << "curr_dis: " << person_list[m - wait_num].curr_dis << "\n";
+                wait_num--;
+            }
+            time++;
+            // cout << "time: " << time << "==================================\n";
+            // for (int i = 0; i < n; i++)
+            // {
+            //     for (int j = 0; j < n; j++)
+            //     {
+            //         cout << map[i][j] << " ";
+            //     }
+            //     cout << "\n";
+            // }
+        }
+        cout << time << "\n";
+    }
+}
